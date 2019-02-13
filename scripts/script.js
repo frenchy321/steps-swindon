2b5457e6f76d8246eae83578a8d3f3ee788deccc
Andy French <stoutstaff@gmail.com>
2019-02-13 19:14:28 +0000

    altered nav bar and deleted some pages and dropdown

--- scripts/script.js
+++ scripts/script.js
@@ -16,8 +16,6 @@
   });
 });*/
 
-// NAV-DROPDOWN1
-
 // HOME-ICONS-SLIDER
 
 $(function() {
@@ -82,4 +80,77 @@ $(function() {
   $("#home-lightbox a").magnificPopup({
     type: "image"
   });
+});
+
+// QUESTIONS-AND-ANSWERS-SECTION
+
+$(function() {
+  $("#questions-and-answers").css({ background: "#2710a7" });
+  $("#questions-and-answers h3").css({ color: "#fff" });
+});
+
+// SPINNER-PLUGIN
+
+(function($) {
+  $.fn.letterDrop = function() {
+    // Chainability
+    return this.each(function() {
+      var obj = $(this);
+
+      var drop = {
+        arr: obj.text().split(""),
+
+        range: {
+          min: 1,
+          max: 9
+        },
+
+        styles: function() {
+          var dropDelays = "\n",
+            addCSS;
+
+          for (i = this.range.min; i <= this.range.max; i++) {
+            dropDelays += ".ld" + i + " { animation-delay: 1." + i + "s; }\n";
+          }
+
+          addCSS = $("<style>" + dropDelays + "</style>");
+          $("head").append(addCSS);
+        },
+
+        main: function() {
+          var dp = 0;
+          obj.text("");
+
+          $.each(this.arr, function(index, value) {
+            dp = dp.randomInt(drop.range.min, drop.range.max);
+
+            if (value === " ") value = "&nbsp"; //Add spaces
+
+            obj.append(
+              '<span class="letterDrop ld' + dp + '">' + value + "</span>"
+            );
+          });
+        }
+      };
+
+      Number.prototype.randomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+
+      // Create styles
+      drop.styles();
+
+      // Initialise
+      drop.main();
+    });
+  };
+})(jQuery);
+
+// USAGE
+$("#spinner h1").letterDrop();
+
+// END-OF-SPINNER-PLUGIN
+
+$(function() {
+  $("#somecomponent").locationpicker();
 });
