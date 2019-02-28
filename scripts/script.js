f81a034b2cbbf4eb92e67f8c95089bf0a8133251
Andy French <stoutstaff@gmail.com>
2019-02-28 09:59:37 +0000

    altered tiles slider size on home page, and changed background color on sections

--- scripts/script.js
+++ scripts/script.js
@@ -65,15 +65,6 @@ $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-$(function() {
-  $("#roll").css({
-    background: "#758da7",
-    color: "white",
-    borderRadius: "1em",
-    width: "auto"
-  });
-});
-
 // HOME-LIGHTBOX
 
 $(function() {
@@ -90,70 +81,8 @@ $(function() {
   $("#questions-and-answers h3").css({ color: "#fff" });
 });
 
-// SPINNER-PLUGIN
-
-(function($) {
-  $.fn.letterDrop = function() {
-    // Chainability
-    return this.each(function() {
-      var obj = $(this);
-
-      var drop = {
-        arr: obj.text().split(""),
-
-        range: {
-          min: 1,
-          max: 9
-        },
-
-        styles: function() {
-          var dropDelays = "\n",
-            addCSS;
-
-          for (i = this.range.min; i <= this.range.max; i++) {
-            dropDelays += ".ld" + i + " { animation-delay: 1." + i + "s; }\n";
-          }
-
-          addCSS = $("<style>" + dropDelays + "</style>");
-          $("head").append(addCSS);
-        },
-
-        main: function() {
-          var dp = 0;
-          obj.text("");
-
-          $.each(this.arr, function(index, value) {
-            dp = dp.randomInt(drop.range.min, drop.range.max);
-
-            if (value === " ") value = "&nbsp"; //Add spaces
-
-            obj.append(
-              '<span class="letterDrop ld' + dp + '">' + value + "</span>"
-            );
-          });
-        }
-      };
-
-      Number.prototype.randomInt = function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-      };
-
-      // Create styles
-      drop.styles();
-
-      // Initialise
-      drop.main();
-    });
-  };
-})(jQuery);
-
-// USAGE
-$("#spinner").letterDrop();
-
-// END-OF-SPINNER-PLUGIN
-
 $(function() {
-  // $("#somecomponent").locationpicker();
+  $("#somecomponent").locationpicker();
 });
 
 // CONTACT-FORM
