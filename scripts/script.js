da8740b5ca18e53a4a73358d1c77011537a2f15a
Andy <andy.french@hotmail.co.uk>
2020-04-10 20:05:01 +0100

    Added covid-19 popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,45 @@
 // HOME-ICONS-SLIDER
 
-$(function() {
-  var items = $("#home-icons i, #home-icons h3");
+$(function () {
+  var items = $('#home-icons i, #home-icons h3');
 
-  items.on("click", function() {
-    $(this)
-      .siblings("div")
-      .slideToggle(1000);
+  items.on('click', function () {
+    $(this).siblings('div').slideToggle(1000);
   });
+});
+
+// COVID-19
+
+const btn = document.querySelector('.covid-btn');
+const covid = document.querySelector('.covid');
+const covidx = document.querySelector('.popup-close');
+
+// DISPLAY COVID-19 POPUP
+
+btn.addEventListener('click', () => {
+  covid.classList.toggle('d-none');
+});
+
+// REMOVE COVID-19 POPUP
+
+covidx.addEventListener('click', () => {
+  covid.style.display = 'none';
+});
+
+covidx.addEventListener('click', () => {
+  covid.style.display = 'none';
+  location.reload(true);
+});
+
+covid.addEventListener('click', () => {
+  covid.style.display = 'none';
+  location.reload(true);
 });
 
 // QUOTE-SLIDER
 
-$(function() {
-  var allQuotes = $(".quote-slider");
+$(function () {
+  var allQuotes = $('.quote-slider');
   var currentQuote = 0;
 
   function changeQuote() {
@@ -33,55 +59,55 @@ $(function() {
 
 // CAROUSELS (HOME AND GROUPS)
 
-$(".carousel").carousel({
-  interval: 4000
+$('.carousel').carousel({
+  interval: 4000,
 });
 
 // Get the current year for the copyright
 
-$("#year span").text(new Date().getFullYear());
+$('#year span').text(new Date().getFullYear());
 
 // TOOLTIP
 
-$(function() {
+$(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
 // HOME-LIGHTBOX
 
-$(function() {
-  $("#home-lightbox a").magnificPopup({
-    type: "image",
-    gallery: { enabled: true }
+$(function () {
+  $('#home-lightbox a').magnificPopup({
+    type: 'image',
+    gallery: { enabled: true },
   });
 });
 
 // GOOGLE-MAP
 
-$(function() {
+$(function () {
   if ($.prototype.locationpicker) {
-    $("#google-map").locationpicker({
+    $('#google-map').locationpicker({
       location: {
         latitude: 51.5655819,
-        longitude: -1.7444095
+        longitude: -1.7444095,
       },
-      enableReverseGeocode: false
+      enableReverseGeocode: false,
     });
   }
 });
 
 // CONTACT-FORM
 
-$(function() {
-  $("#contact-form").submit(function(e) {
+$(function () {
+  $('#contact-form').submit(function (e) {
     var textInputs = $(this).find("input[type='text'], textarea");
     let isValid = true;
-    textInputs.each(function(index) {
-      if ($(this).val() === "") {
-        $(this).css({ border: "1px solid red" });
+    textInputs.each(function (index) {
+      if ($(this).val() === '') {
+        $(this).css({ border: '1px solid red' });
         isValid = false;
       } else {
-        $(this).css({ border: "none" });
+        $(this).css({ border: 'none' });
       }
     });
 
